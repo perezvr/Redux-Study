b4b65955cb03fa68d41c90f8a8f9e3eb30242823
Renan <perez.vr@gmail.com>
2020-05-20 00:14:29 -0300

    estilização do header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
+import GlobalStyle from './styles/global';
+import Header from './components/Header';
+import Routes from './routes';
+
+/**
+ * Como o header será clicável e fará parte da navegação, ele também precisa estar dentro
+ * do BrowserRouter
+ */
 function App() {
-  return <h1>Hello Redux</h1>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes />
+      <GlobalStyle />
+    </BrowserRouter>
+  );
 }
 
 export default App;
