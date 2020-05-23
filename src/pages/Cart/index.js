60897c14e74d7db3e552ef2c41486fbcc4b01336
Renan <perez.vr@gmail.com>
2020-05-23 00:00:54 -0300

    redux para o carrinho de compras

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import {
   MdRemoveCircleOutline,
   MdAddCircleOutline,
@@ -7,7 +8,7 @@ import {
 
 import { Container, ProductTable, Total } from './styles';
 
-export default function Cart() {
+function Cart({ cart }) {
   return (
     <Container>
       <ProductTable>
@@ -21,37 +22,36 @@ export default function Cart() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>
-              <img
-                src="https://static.netshoes.com.br/produtos/tenis-olympikus-breed-2/12/D22-3195-012/D22-3195-012_zoom1.jpg?ts=1574458867&?ims=544xhttps://static.netshoes.com.br/produtos/tenis-olympikus-breed-2/12/D22-3195-012/D22-3195-012_zoom1.jpg?ts=1574458867&?ims=1088x"
-                alt="Tênis"
-              />
-            </td>
-            <td>
-              <strong>Tenis muito massa</strong>
-              <span>R$ 129,90</span>
-            </td>
-            <td>
-              <div>
+          {cart.map((product) => (
+            <tr>
+              <td>
+                <img src={product.image} alt={product.title} />
+              </td>
+              <td>
+                <strong>{product.title}</strong>
+                <span>{product.priceFormatted}</span>
+              </td>
+              <td>
+                <div>
+                  <button type="button">
+                    <MdRemoveCircleOutline size={20} color="#7159c1" />
+                  </button>
+                  <input type="number" readOnly value={product.amount} />
+                  <button type="button">
+                    <MdAddCircleOutline size={20} color="#7159c1" />
+                  </button>
+                </div>
+              </td>
+              <td>
+                <strong>R$258,8</strong>
+              </td>
+              <td>
                 <button type="button">
-                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                  <MdDelete size={20} color="#7159c1" />
                 </button>
-                <input type="number" readOnly value={1} />
-                <button type="button">
-                  <MdAddCircleOutline size={20} color="#7159c1" />
-                </button>
-              </div>
-            </td>
-            <td>
-              <strong>R$258,8</strong>
-            </td>
-            <td>
-              <button type="button">
-                <MdDelete size={20} color="#7159c1" />
-              </button>
-            </td>
-          </tr>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </ProductTable>
 
@@ -66,3 +66,13 @@ export default function Cart() {
     </Container>
   );
 }
+
+/**
+ * Função padrão para mapear um estado do redux para as propriedades do componente
+ * @param state state do Redux
+ */
+const mapsStateToProps = (state) => ({
+  cart: state.cart,
+});
+
+export default connect(mapsStateToProps)(Cart);
--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -10,7 +10,17 @@ export default function cart(state = [], action) {
    */
   switch (action.type) {
     case 'ADD_TO_CART':
-      return [...state, action.product];
+      /**
+       * Repetimos o estado anterior, incluindo um novo objeto
+       * Este objeto pega o action.product e inclui a informação amount
+       */
+      return [
+        ...state,
+        {
+          ...action.product,
+          amount: 1,
+        },
+      ];
     default:
       return state;
   }
