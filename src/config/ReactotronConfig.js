ae90de7cd9fbbcc9f031b000190b5055d85c5f7e
Renan <perez.vr@gmail.com>
2020-06-08 22:19:37 -0300

    Reactotron Redux Saga

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,12 +1,15 @@
 import Reactotron from 'reactotron-react-js';
 import { reactotronRedux } from 'reactotron-redux';
-
+import reactotronSaga from 'reactotron-redux-saga';
 /**
  * O create-react-app cria essa variável 'node_env' que pode ser verificada para
  * saber se estamos em ambiente de desenvolvimento
  */
 if (process.env.NODE_ENV === 'development') {
-  const tron = Reactotron.configure().use(reactotronRedux()).connect();
+  const tron = Reactotron.configure()
+    .use(reactotronRedux())
+    .use(reactotronSaga())
+    .connect();
 
   /* Limpa o log em toda inicialização */
   tron.clear();
--- src/store/index.js
+++ src/store/index.js
@@ -7,7 +7,12 @@ import createSagaMiddleware from 'redux-saga';
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
 
-const sagaMiddleware = createSagaMiddleware();
+const sagaMonitor =
+  process.env.NODE_ENV === 'development'
+    ? console.tron.createSagaMonitor()
+    : null;
+
+const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
 
 /* Integrando redux ao Reactotron */
 const enhancer =
